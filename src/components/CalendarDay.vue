5dd35e960afd53f8a0a4888af457e725c20eb48e
T <tomasz.kwiatkowski@imaginet.co.uk>
2021-10-09 11:31:15 +0100

    fixed tabindex for none visble days

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -183,9 +183,9 @@ export default {
     },
     dayContentProps() {
       let tabindex;
-      if (!this.day.isDisabled) {
+      if (!this.day.isDisabled && this.inMonth) {
         tabindex = '0';
-      } else if (this.day.isDisabled || this.day.inMonth) {
+      } else {
         tabindex = '-1';
       }
       return {
